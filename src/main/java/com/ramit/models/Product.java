dfc4f82cfd86040e81d5a0c8cd12e4da9c1abc6d
Bhargav <rambhargav017@gmail.com>
2025-02-01 13:23:00 +0530

    test commit

--- src/main/java/com/ramit/models/Product.java
+++ src/main/java/com/ramit/models/Product.java
@@ -13,7 +13,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
-
+// some test
 @Entity
 @Data
 public class Product {
